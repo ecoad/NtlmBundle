dd4f4f403ff7fa35d98b9a3cd8dadefb1d70d245
Elliot Coad <elliotcoad@gmail.com>
2011-11-29 12:33:28 +0000

    Updated whitelist auth

--- Security/Authentication/Provider/NtlmProtocolAuthenticationProvider.php
+++ Security/Authentication/Provider/NtlmProtocolAuthenticationProvider.php
@@ -54,6 +54,7 @@ class NtlmProtocolAuthenticationProvider implements AuthenticationProviderInterf
 
     public function authenticate(TokenInterface $token)
     {
+
         if (!$this->isRemoteAddressAuthorised($_SERVER['REMOTE_ADDR'])) {
             throw new AuthenticationException('NTLM cannot authetnicate against unauthorised IP addresses');
         }
@@ -83,54 +84,14 @@ class NtlmProtocolAuthenticationProvider implements AuthenticationProviderInterf
 
     public function checkNtlm()
     {
-        // loune 25/3/2006, updated 22/08/2009
-        // For more information see:
-        // http://siphon9.net/loune/2007/10/simple-lightweight-ntlm-in-php/
+        $ldapRequest = $this->container->get('ntlm.request');
 
-        $headers = apache_request_headers();
 
-        if (!isset($headers['Authorization'])){
-            header('HTTP/1.1 401 Unauthorized');
-            header('WWW-Authenticate: NTLM');
-            exit;
+        $username = $ldapRequest->ntlm_prompt("testwebsite", "testdomain", "mycomputer", "testdomain.local", "mycomputer.local", "get_ntlm_user_hash");
+        if (!$username) {
+            throw new AuthenticationException('The NTLM authentication failed');
         }
-
-        $auth = $headers['Authorization'];
-
-        if (substr($auth,0,5) == 'NTLM ') {
-            $msg = base64_decode(substr($auth, 5));
-            if (substr($msg, 0, 8) != "NTLMSSP\x00")
-                die('error header not recognised');
-
-            if ($msg[8] == "\x01") {
-                $msg2 = "NTLMSSP\x00\x02\x00\x00\x00".
-                    "\x00\x00\x00\x00". // target name len/alloc
-                    "\x00\x00\x00\x00". // target name offset
-                    "\x01\x02\x81\x00". // flags
-                    "\x00\x00\x00\x00\x00\x00\x00\x00". // challenge
-                    "\x00\x00\x00\x00\x00\x00\x00\x00". // context
-                    "\x00\x00\x00\x00\x00\x00\x00\x00"; // target info len/alloc/offset
-
-                header('HTTP/1.1 401 Unauthorized');
-                header('WWW-Authenticate: NTLM '.trim(base64_encode($msg2)));
-                exit;
-            }
-            else if ($msg[8] == "\x03") {
-                function get_msg_str($msg, $start, $unicode = true) {
-                    $len = (ord($msg[$start+1]) * 256) + ord($msg[$start]);
-                    $off = (ord($msg[$start+5]) * 256) + ord($msg[$start+4]);
-                    if ($unicode)
-                        return str_replace("\0", '', substr($msg, $off, $len));
-                    else
-                        return substr($msg, $off, $len);
-                }
-                $username = get_msg_str($msg, 36);
-                $domain = get_msg_str($msg, 28);
-                $workstation = get_msg_str($msg, 44);
-
-                return $username;
-            }
-        }
+        return $username;
     }
 
     public function isRemoteAddressAuthorised($remoteAddress)
