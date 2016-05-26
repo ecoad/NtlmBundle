5f1faa94711699a14e08e95742e6103dba1e34ac
gggeek <ggiunta@kaliop.com>
2016-05-26 17:39:43 +0100

    Make sure that the security listeners are active (needed with Sf 2.8, probably earlier)

--- BrowserCreativeNtlmBundle.php
+++ BrowserCreativeNtlmBundle.php
@@ -2,7 +2,19 @@
 namespace BrowserCreative\NtlmBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use BrowserCreative\NtlmBundle\DependencyInjection\Security\Factory\NtlmProtocolFactory;
+use BrowserCreative\NtlmBundle\DependencyInjection\Security\Factory\NtlmFormLoginFactory;
 
 class BrowserCreativeNtlmBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        // register the factories which set up the custom auth providers
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new NtlmProtocolFactory());
+        $extension->addSecurityListenerFactory(new NtlmFormLoginFactory());
+    }
 }
